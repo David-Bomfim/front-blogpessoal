c75991d4af64668d5372c7e119ef5c691354f4e3
David Bomfim <davidrochabomfim6@gmail.com>
2024-02-18 19:55:30 -0300

    Rotas criadas

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home';
+import Navbar from './components/NavBar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
+}
+export default App;
