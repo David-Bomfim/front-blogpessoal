1b6b6dbe168420ffbbcedb1c40c3229494ab0acd
David Bomfim <davidrochabomfim6@gmail.com>
2024-02-22 15:03:38 -0300

    Construido a lógica do Componente Formulário Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Navbar from './components/NavBar/Navbar';
+import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
@@ -10,8 +10,6 @@ import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
-import DeletarTema from './components/temas/deletarTema/DeletarTema';
-import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
 
 
 function App() {
@@ -29,8 +27,7 @@ function App() {
               <Route path="/temas" element={<ListaTemas />} />
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
-              <Route path="/deletarTema/:id" element={<DeletarTema />} />
-              <Route path="/postagens" element={<ListaPostagens />} />
+
             </Routes>
           </div>
           <Footer />
